7a345cf5efcf97f433e431975d319fb04bdecff2
P. Colin Manikoth <pmanikoth@ewu.edu>
2014-10-13 23:38:51 -0700

    == REDESIGN ==
    * Updated Zurb Foundation
    * New design

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
+// Foundation
 $(document).foundation();
 
-var t = new Trianglify();
-var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
-document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
+// My Scripts
+$(document).ready(function(){
+
+  
+
+});
